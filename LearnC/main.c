f4c45cff3d98b3cba349a492c0d3c5be39e28853
Charles Wade <charleswade@chucks-mac.home>
2013-07-08 07:29:07 -0400

    Initial Commit

---
+++ LearnC/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  LearnC
+//
+//  Created by Charles Wade on 7/8/13.
+//  Copyright (c) 2013 CW Computing. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
